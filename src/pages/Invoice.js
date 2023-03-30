f38cc8a26073ea68e1509fd2498717149bd12e4f
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2023-03-30 04:37:38 +0200

    invoice

--- src/pages/Invoice.js
+++ src/pages/Invoice.js
@@ -1,9 +1,57 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { Card, Col, Row } from 'react-bootstrap'
+import Charts2 from 'components/dashboard/Chart2'
+import Charts1 from 'components/dashboard/Charts1'
+import InfoCard from 'components/dashboard/InfoCard'
+import DataTableDT from 'components/DataTableDT'
+import { dashboardData, ProductsColumnsDB } from 'static'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchProducts } from 'redux/lib/products'
+import Donut from 'components/dashboard/Dount'
+import Chart from 'components/dashboard/Chart3'
+import Chartt from 'components/dashboard/Chart3'
 const Invoice = () => {
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    dispatch(fetchProducts())
+  }, [dispatch])
+
+  const products = useSelector(state => state?.products?.products[0]?.products)
+  console.log(products);
+  const {cards} = dashboardData
   return (
-    <div>Invoice</div>
+    <div>
+    <h2 className='fw-bold mb-3 text-dark'>Invoice</h2>
+      <Row>
+        {
+          cards.map(card => {
+            return(
+              <Col key={card.id} md='6' lg='3'>
+              <InfoCard {...card} />
+            </Col>
+            )
+          })
+        }
+        <Col md='6' className='mb-3'>
+          <Card className='border-0 shadow-sm'>
+          <Card.Body>
+          <h4>Reports</h4>
+              <Donut/>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md='6' className='mb-3'>
+          <Card className='border-0 shadow-sm'>
+          <Card.Body>
+              <h4>Analytics</h4>
+              <Chartt/>
+            </Card.Body>
+          </Card>
+        </Col>
+       
+      </Row>
+    </div>
   )
 }
 
-export default Invoice
+export default Invoice;
